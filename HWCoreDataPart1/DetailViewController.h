274a447b73d11229ffdab348d426d5f9314bc8f0
Ильдар Залялов <Zalyalov.Ild@gmail.com>
2016-12-12 13:52:13 +0300

    Initial Commit

---
+++ HWCoreDataPart1/DetailViewController.h
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.h
+//  HWCoreDataPart1
+//
+//  Created by Ildar Zalyalov on 12.12.16.
+//  Copyright © 2016 com.itis.iosLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "HWCoreDataPart1+CoreDataModel.h"
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) Event *detailItem;
+@property (weak, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
+
---
+++ HWCoreDataPart1/MasterViewController.h
@@ -0,0 +1,24 @@
+//
+//  MasterViewController.h
+//  HWCoreDataPart1
+//
+//  Created by Ildar Zalyalov on 12.12.16.
+//  Copyright © 2016 com.itis.iosLab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+#import "HWCoreDataPart1+CoreDataModel.h"
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController<Event *> *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
